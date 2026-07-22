77aee298f2ea0e7ca5ac831b0600a6c16f3f9807
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-22 03:57:19 +0100

    refactor(navbar): add theme-specific logo variants

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,15 +1,59 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { NavBar } from "@/components/landing/nav-bar";
 import { ThemePicker } from "../theme-picker";
+import { useTheme } from "next-themes";
+
+function getActiveEffectButton(theme?: string) {
+  switch (theme) {
+    case "design":
+      return "/activeButton-Design.svg";
+
+    case "multimedia":
+      return "/activeButton-Multimedia.svg";
+
+    case "communication":
+      return "/activeButton-Communication.svg";
+
+    case "marketing":
+      return "/join-button.svg";
+
+    case "default":
+    default:
+      return "/join-button.svg";
+  }
+}
+function getActiveEffectLogo(theme?: string) {
+  switch (theme) {
+    case "design":
+      return "/activeLogo-Design.svg";
+
+    case "multimedia":
+      return "/activeLogo-Multimedia.svg";
+
+    case "communication":
+      return "/activeLogo-Communication.svg";
+
+    case "marketing":
+      return "/activeLogo-Marketing.svg";
+
+    case "default":
+    default:
+      return "/nav-logo.svg";
+  }
+}
 
 export function Header() {
+  const { theme } = useTheme();
+  const activeButton = getActiveEffectButton(theme);
+  const activeLogo = getActiveEffectLogo(theme);
   return (
     <header className="absolute top-0 left-0 right-0 z-50 w-full  bg-[#000000]">
       <div className="mx-auto flex h-23.75 max-w-6xl items-center gap-25 px-4 sm:px-6 lg:px-8">
         <Link href="/">
           <Image
-            src="/nav-logo.svg"
+            src={activeLogo}
             alt="Brandiha"
             width={253}
             height={62}
@@ -20,7 +64,7 @@ export function Header() {
         <ThemePicker/>
         <Link href="/register">
           <Image
-            src="/join-button.svg"
+            src={activeButton}
             alt="Join"
             width={211}
             height={69}
