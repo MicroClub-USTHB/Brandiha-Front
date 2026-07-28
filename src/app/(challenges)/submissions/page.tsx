6be79ae47b4f20c622264e353155eee0303efd6b
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-28 16:18:28 +0100

    style: update grid style

--- src/app/(challenges)/submissions/page.tsx
+++ src/app/(challenges)/submissions/page.tsx
@@ -7,7 +7,7 @@ export default async  function submissionChallenge() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-5xl font-heading text-white font-bold mb-4">Challenges</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             {challenges.map((challenge) => (
                 <Link key={challenge.id} href={`/submissions/${challenge.id}`}>
                     <ChallengeCard
