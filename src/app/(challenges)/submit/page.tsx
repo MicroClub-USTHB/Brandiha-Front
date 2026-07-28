ac1566888b5698e0fe1146d762960f2f162ce48d
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-28 06:02:56 +0100

    feat:add submissions routes

--- src/app/(challenges)/submit/page.tsx
+++ src/app/(challenges)/submit/page.tsx
@@ -1,10 +1,24 @@
-import ChallengesGrid from "@/components/challenges-grid";
-
-export default function submitChallenge() {  
+import Link from "next/link";
+import {getPublicChallenges } from "@/lib/api/challenges";
+import ChallengeCard, { Department } from "@/components/challenge-card";
+export default async  function submitChallenge() {  
+    const challenges = await getPublicChallenges();
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-5xl font-heading text-white font-bold mb-4">Challenges</h1>
-            <ChallengesGrid link="#"/>
+            <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-2 gap-4">
+            {challenges.map((challenge) => (
+                <Link key={challenge.id} href={`/submit/${challenge.id}`}>
+                    <ChallengeCard
+                        key={challenge.id}
+                        department={challenge.department as  Department}
+                        title={challenge.title}
+                        unlocks_at={challenge.unlocks_at}
+                        ends_at={challenge.ends_at}
+                    />
+                </Link>
+            ))}
+            </div>
         </div>
     );
 }
