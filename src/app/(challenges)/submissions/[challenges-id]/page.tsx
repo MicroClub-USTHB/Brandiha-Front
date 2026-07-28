28b9c5480c50e9f249fef6fa3be9ec517c8cb894
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-28 06:07:52 +0100

    fix build error

---
+++ src/app/(challenges)/submissions/[challenges-id]/page.tsx
@@ -0,0 +1,7 @@
+export default function SubmitChallenge() {   
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-5xl font-heading text-white font-bold mb-4">Challenges</h1>     
+        </div>
+    );
+}
