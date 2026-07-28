f61bc04025472664815870ec28e2f55187ea4064
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-28 05:48:46 +0100

    fix: type challenge department with Department enum

--- src/app/(challenges)/submit/page.tsx
+++ src/app/(challenges)/submit/page.tsx
@@ -4,7 +4,7 @@ export default function submitChallenge() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-5xl font-heading text-white font-bold mb-4">Challenges</h1>
-            <ChallengesGrid />
+            <ChallengesGrid link="#"/>
         </div>
     );
 }
