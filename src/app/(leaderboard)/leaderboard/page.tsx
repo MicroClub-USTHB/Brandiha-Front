150d85328b5e4667d48e92bc5d29162c55e93f17
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-29 06:28:53 +0100

    style: add mobile responsive styles for leaderboard

--- src/app/(leaderboard)/leaderboard/page.tsx
+++ src/app/(leaderboard)/leaderboard/page.tsx
@@ -26,7 +26,7 @@ export default async function Leaderboard() {
   }
   return (
     <div className="flex flex-col items-center justify-start gap-2 min-h-screen">
-      <h1 className="text-8xl font-heading text-white font-bold mb-4">
+      <h1 className="text-5xl lg:text-8xl font-heading text-white font-bold mb-4">
         Leaderboard
       </h1>
       <LeaderboardComponent leaderboardData={sortedLeaderboardData} />
--- src/components/leaderboard/leaderboard-row.tsx
+++ src/components/leaderboard/leaderboard-row.tsx
@@ -9,24 +9,24 @@ interface LeaderboardRowProps {
 
 export default function LeaderboardRow({rank,teamName,score} : LeaderboardRowProps) {
     return (
-        <div className="w-200 h-15 2xl:w-290 2xl:h-25  bg-[#E9DFD4] py-2 px-4 flex  items-center justify-between mb-2">
+        <div className="w-90 h-15 lg:w-250 lg:h-15 2xl:w-290 2xl:h-25  bg-[#E9DFD4] py-2 px-4 flex  items-center justify-between mb-2">
           <div className="flex flex-row items-center justify-start gap-4">
             {rank === 1 && <Image height={22} width={22} src="/gold.svg" alt="Gold Medal" className="w-22 h-22 2xl:w-35 2xl:h-35 mt-2"/>}
             {rank === 2 && <Image height={22} width={22} src="/silver.svg" alt="Silver Medal" className="w-22 h-22 2xl:w-35 2xl:h-35 mt-2"/>}
             {rank === 3 && <Image height={22} width={22} src="/bronze.svg" alt="Bronze Medal" className="w-22 h-22 2xl:w-35 2xl:h-35 mt-2"/>}
             {rank > 3 && 
-              <span className="text-5xl 2xl:text-7xl lg:mb-3 2xl:mb-3 h-22 w-22 2xl:w-35 2xl:h-35 flex items-center justify-center font-heading font-bold text-center text-black">
+              <span className="text-xl lg:text-5xl 2xl:text-7xl lg:mb-3 2xl:mb-3 h-22 w-22 2xl:w-35 2xl:h-35 flex items-center justify-center font-heading font-bold text-center text-black">
                 {rank}
               </span>
             }
             <h1 className=" ml-2 flex items-center justify-center">
-              <span className="text-5xl 2xl:text-7xl lg:mb-3 2xl:mb-3 font-heading text-black font-bold ">
+              <span className="text-xl lg:text-5xl 2xl:text-7xl lg:mb-3 2xl:mb-3 font-heading text-black font-bold ">
                 {teamName}
               </span>
             </h1>
           </div>
-          <h1 className="h-full w-40 2xl:w-60 2xl:h-35  text-center text-black bg-[url('/timer-default.svg')] bg-contain bg-center bg-no-repeat flex items-center justify-center">
-            <span className="text-4xl 2xl:text-7xl lg:mb-3 2xl:mb-3 font-heading text-black font-bold ">
+          <h1 className="h-full w-20 lg:w-40 2xl:w-60 2xl:h-35  text-center text-black bg-[url('/timer-default.svg')] bg-contain bg-center bg-no-repeat flex items-center justify-center">
+            <span className="text-xl lg:text-4xl 2xl:text-7xl lg:mb-3 2xl:mb-3 font-heading text-black font-bold ">
                 {score}
             </span>
           </h1>
