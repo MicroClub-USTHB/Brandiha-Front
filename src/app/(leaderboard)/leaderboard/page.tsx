9cabce99e6be8985559d179d119b5cd22fdec8a1
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2026-07-30 04:21:52 +0100

    chore: update leaderboard route style

--- src/app/(leaderboard)/leaderboard/page.tsx
+++ src/app/(leaderboard)/leaderboard/page.tsx
@@ -18,12 +18,11 @@ export default async function Leaderboard() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-start gap-4 p-6">
-      <h1 className="text-6xl font-bold font-heading text-white">Leaderboard</h1>
-
-      {leaderboardResponse.frozen && (
-        <div className="w-full max-w-7xl rounded-lg bg-cyan-500/20 px-4 py-2 text-cyan-200 border border-cyan-500/40 text-sm text-center">
-          ❄️ Les scores sont actuellement gelés !
-        </div>
+      {leaderboardResponse.frozen ? (
+        <h1 className="mb-4 text-8xl font-bold font-heading text-white">Leaderboard (frozen)</h1>
+      ) :(
+        <h1 className="mb-4 text-8xl font-bold font-heading text-white">Leaderboard</h1>
+      
       )}
 
       <LeaderboardComponent leaderboardData={sortedLeaderboardData} />
